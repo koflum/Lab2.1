070e183447e9b33c7300d36c9fa24993347efffe
koflum <lkw_xoras@hotmail.com>
2017-11-07 09:59:58 +0800

    commit

---
+++ app/src/main/java/my/edu/tarc/practical21/ProfileActivity.java
@@ -0,0 +1,37 @@
+package my.edu.tarc.practical21;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class ProfileActivity extends AppCompatActivity {
+    private EditText editTextName, editTextEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        editTextName = (EditText)findViewById(R.id.editTextName);
+        editTextEmail =(EditText)findViewById(R.id.editTextEmail);
+    }
+
+    public void saveProfile(View view)
+    {
+        Intent intent = getIntent();
+
+        //Obtain input data from user
+        String name, email;
+        name = editTextName.getText().toString();
+        email =editTextEmail.getText().toString();
+        intent.putExtra(MainActivity.PROFILE_NAME, name);
+        intent.putExtra(MainActivity.PROFILE_EMAIL, email);
+
+        //Return result to the Main Activity
+        setResult(MainActivity.REQUEST_PFOFILE_UPDATE, intent);
+        finish();
+
+    }
+}
